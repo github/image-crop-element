97fbc4c679f9b5d382043a5555e71f73de819449
Keith Cirkel <keithamus@users.noreply.github.com>
2019-09-12 11:29:23 +0100

    types(ts): add window declaration

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,9 @@ export default class ImageCropElement extends HTMLElement {
   src: string || null
   loaded: boolean
 }
+
+declare global {
+  interface Window {
+    ImageCropElement: typeof ImageCropElement
+  }
+}
